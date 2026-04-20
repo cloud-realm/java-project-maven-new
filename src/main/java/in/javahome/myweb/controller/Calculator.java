738bedcf8d3bdeff032381fd6da0f939e531f4b3
cloud-realm <ankit.kas28@gmail.com>
2026-04-20 13:21:50 +0530

    Refactor multiply method parameter names

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -12,8 +12,8 @@ public class Calculator {
 	public int add(int i, int j){
 		return i+j;
 	}
-	public int multiply(int a, int b){
-		return a*b;
+	public int multiply(int i, int j){
+		return i*j;
 	}
 	
 	public int sub(int i, int j){
